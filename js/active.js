f60400c239f3451cd5870c4ade7d3d16f7aa1bea
鱼宁 <1578517699@qq.com>
2017-08-12 02:12:24 +0800

    修改

--- js/active.js
+++ js/active.js
@@ -6,6 +6,7 @@ function active_content() {
     var creat = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ThoughtCoding实验室由若干智能硬件爱好者于2015年4月共同创办。短短数月，在老师的指导下，嵌入式、大数据、安卓、web组共同协作，参加了互联网+、大挑、3S杯等大赛，并获得出色的成绩。同时有项目已经进驻西邮创业孵化基地。<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;实验室目前拥有最新pcduino开发板，一百多种传感器， 30多台主机搭建的hadoop云计算集群。实验室研究方向为:嵌入式、Web前端、移动（Android）端、运维服务器（Linux）、大数据。组内学长学姐可带领学习。我们相信，技术改变生活，奋斗共享价值。实验室期待和你一起，共同创造更多可能。<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;实验室定期积极讨论技术热点问题，并开启相关项目，让大家在开展项目的过程中磨练技术、促进合作、了解趋势，在项目驱动下大家的学习劲头旺盛，对学习的技术有了更多的应用、更多的了解。<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;以下是工作室项目一览表： <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1.基于hadoop的多生理参数的远程监护系统 硬件部分采集人体的心电、脉搏、血氧、血糖、脑电等各项生理参数，Android端实时显示并上传至云服务器。后台经过大数据分析，给医生提供详细的临床数据，给患者就医提供方便。系统采用可穿戴智能硬件设计跌倒检测装置，为空巢老年人出行提供安全保障。<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 2.智能四旋翼无人机 基于STM32开发板，利用PID算法，调节PWM，控制四旋翼自动完成起飞、悬停、寻迹以及降落等功能。<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 3.智能搜救机器人 通过Android端控制智能搜救机器人完成指定动作，获得机器人的实时视频数据，完成既定任务。<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 4.基于安卓系统的远程安防智能家居系统 使用搭载安卓系统的Pcduino开发板作为智能家居控制平台，利用光、热、烟雾、声音等传感器采集数据，用户通过手机APP获取到实时信息以监控室内情况，来保障家用电器的正常使用。<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 5. 基于Android平台的城市地标移动搜索开发 旅游业是陕西省的支柱产业，开发和旅游产业相关的移动视觉搜索应用具有重要的学术和应用价值。本项目研究基于移动视觉搜索的城市地标图像识别技术。通过城市地标的识别可以提供给游客更好的旅游体验，进一步挖掘城市的旅游资源，提升城市的形象。";
     var as = document.getElementsByTagName("a");
     var ps = document.getElementsByTagName("p");
+    change = new Array(false);
     for (var i = 0; i < as.length; i++) {
         (function(i) {
 
@@ -27,3 +28,42 @@ function active_content() {
     }
 }
 addLoadEvent(active_content);
+/*
+function active_content() {
+    var ai = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;毫无疑问，人工智能技术已经成为全球科技巨头的宠儿，面临转型挑战的联想同样将目光转向AI。<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;在联想2017全球创新科技大会(Tech World)上，联想集团董事长兼CEO杨元庆表示，“人工智能让这个问号不再存在，新世界会成为现实。而联想的使命就是要做人工智能的使能者、推动者。”<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;什么是人工智能呢？<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;根基百度百科上面的说法，它是研究、开发用于模拟、延伸和扩展人的智能的理论、方法、技术及应用系统的一门新的技术科学。<br>人工智能可以对人的意识、思维的信息过程的模拟。人工智能不是人的智能，但能像人那样思考、也可能超过人的智能。<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;人工智能的应用领域是什么呢？<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;机器翻译是人工智能的重要分支和最先应用领域。不过就已有的机译成就来看，机译系统的译文质量离终极目标仍相差甚远；而机译质量是机译系统成败的关键。中国数学家、语言学家周海中教授曾在论文《机器翻译五十年》中指出：要提高机译的质量，首先要解决的是语言本身问题而不是程序设计问题；单靠若干程序来做机译系统，肯定是无法提高机译质量的；另外在人类尚未明了大脑是如何进行语言的模糊识别和逻辑判断的情况下，机译要想达到“信、达、雅”的程度是不可能的。智能家居之后，人工智能成为家电业的新风口，而长虹正成为将这一浪潮掀起的首个家电巨头。长虹发布两款CHiQ智能电视新品，主打手机遥控器、带走看、随时看、分类看功能。<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;我们实验室即时看到了人工智能的发展前景，像大家提供这方面的资源，对这方面有兴趣的成员可以报名参加每周的网络课程。<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;从2017年7月24号晚上8：30开始第一节课程，之后会在每周一晚上8点半进行人工智能的学习。";
+    var share = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;TCP/IP协议是一个协议集合。大家叫的时候方便说，所以统称为TCP/IP。TCP/IP协议族中有一个重要的概念是分层，TCP/IP协议按照层次分为四层。<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;要想了解什么是TCP/IP协议，就要知道为什么有这个协议。中国人和中国人说话，要遵循汉语的的语法结构，使用汉语的发音。当我们和外国人交流时，就要适用外国的语言了，遵循外国的语法机构和发音。其实这就是一种协议，只不过我们称之为语言。计算机再这方面体现的更为直观，两台机器之间该如何通信呢，就需要制定各种各样的协议了。例如：文件传输适用TCP协议。域名系统适用DNS协议。有了些协议的存在，各种数据流按照规则传输，计算机之间得意通信。<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;那什么是正则表达式呢？<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;正则表达式是对字符串操作的一种逻辑公式，就是用事先定义好的一些特定字符、及这些特定字符的组合，组成一个“规则字符串”，这个“规则字符串”用来表达对字符串的一种过滤逻辑。正则表达式是一种特殊的字符串模式，用于匹配一组字符串，就好比用模具做产品，而正则就是这个模具，定义一种规则去匹配符合规则的字符。<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;在2017年5月16日晚上8点，实验室的学长学姐向大家讲了这两方面的知识，然后进行了问答与讨论，都学到了很多。";
+    var fuhua = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;亢老师自豪地向记者介绍，近年来他指导学生参加“飞思卡尔杯”全国大学生智能车竞赛、全国大学生电子设计竞赛、大学生“TI杯”模拟及模数混合电路应用设计竞赛、“挑战杯”大学生课外学术科技作品竞赛、“美新杯”物联网大赛、“未来伙伴杯”中国智能机器人大赛、“博创杯”大学生嵌入式大赛等各类比赛，共获得国家级奖6项，省级奖30余项，累计指导学生百余人。随后，亢老师为我们分享了近年来他在指导学生科技创新方面所总结的经验。<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;亢红波老师继续谈到：“要善于利用各种资源和渠道，获取自己需要的信息。作为学生，就应该充分利用图书馆、专业网站、论坛、QQ群、社团等，另外同学也是我们的第二老师，能提供有用信息的地方就存在着价值。”亢红波老师讲述了孵化基地项目的由来：“在同朋友聊天时，对其所从事的技术问题进行了探讨。在实地参观时，对设备的结构与工作原理有了更深层次的理解，还在维修现场参与了问题的讨论，提出了我的观点，并与第一线技术人员互留了联系方式。合作是从维修开始，后续又帮他们解决了几个问题，由此建立了信任关系。”正是这样才有了亢红波老师孵化基地的研究项目。<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;亢红波老师大部分时间都是和学生们在实验室里，不仅在工程实践上悉心指导学生，更在生活中热心关爱学生。张芊说：“当我们有问题和疑惑的时候，亢老师都会第一时间帮我们解惑；在准备项目的过程中，亢老师更是彻夜指导我们。寒暑假期间，老师热心为我们安排住宿，甚至让部分同学住在自己家里。每到毕业季的时候，亢老师都会找往届学长学姐们对我们进行就业指导，并向企业进行内部推荐。”亢红波老师常说：“现在是师生，出校是朋友。”因而，学生毕业后遇到生活方面问题也都会找他谈心。正如学生们所说的：“亢红波老师的团队是一个温暖的大家庭，虽短短几年却值得铭记。”";
+    var girl = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;11月26日晚，计算机学院与ThoughtWorks联合创新实验室暨卓越女生计划启动仪式在长安校区举行。<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ThoughtWorks公司胡皓、牛亚萌，计算机院党委书记葛茂、陈莉君教授及各学院女生参加仪式。<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;仪式上计算机学院党委书记葛茂对ThoughtWorks公司的卓越女生计划表示高度赞赏，勉励女同学增强自信，努力追求卓越。计算机学院陈莉君教授也对同学们提出了殷切希望，希望大家通过努力，成就美好未来。ThoughtWorks公司女工程师牛亚萌关于软件开发工程师日常工作的分享，让现场女生感受到自己很重要。王妮的“to be more”给同学们积极向上无所畏惧的动力。ThoughtWorks公司卓越女生计划负责人胡皓对卓越女生计划实验班的教学安排进行了详细解读，并现场回答了同学们关心的问题。<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  现场同学们对联合培养计划表现出极大地热情，纷纷表示要参加实验班，提高自身实践能力，增强就业竞争力。<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; “卓越女生计划”实验班是计算机学院进行创新教育的又一次实践尝试，以此提高女生的就业竞争力。";
+    var creat = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ThoughtCoding实验室由若干智能硬件爱好者于2015年4月共同创办。短短数月，在老师的指导下，嵌入式、大数据、安卓、web组共同协作，参加了互联网+、大挑、3S杯等大赛，并获得出色的成绩。同时有项目已经进驻西邮创业孵化基地。<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;实验室目前拥有最新pcduino开发板，一百多种传感器， 30多台主机搭建的hadoop云计算集群。实验室研究方向为:嵌入式、Web前端、移动（Android）端、运维服务器（Linux）、大数据。组内学长学姐可带领学习。我们相信，技术改变生活，奋斗共享价值。实验室期待和你一起，共同创造更多可能。<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;实验室定期积极讨论技术热点问题，并开启相关项目，让大家在开展项目的过程中磨练技术、促进合作、了解趋势，在项目驱动下大家的学习劲头旺盛，对学习的技术有了更多的应用、更多的了解。<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;以下是工作室项目一览表： <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1.基于hadoop的多生理参数的远程监护系统 硬件部分采集人体的心电、脉搏、血氧、血糖、脑电等各项生理参数，Android端实时显示并上传至云服务器。后台经过大数据分析，给医生提供详细的临床数据，给患者就医提供方便。系统采用可穿戴智能硬件设计跌倒检测装置，为空巢老年人出行提供安全保障。<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 2.智能四旋翼无人机 基于STM32开发板，利用PID算法，调节PWM，控制四旋翼自动完成起飞、悬停、寻迹以及降落等功能。<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 3.智能搜救机器人 通过Android端控制智能搜救机器人完成指定动作，获得机器人的实时视频数据，完成既定任务。<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 4.基于安卓系统的远程安防智能家居系统 使用搭载安卓系统的Pcduino开发板作为智能家居控制平台，利用光、热、烟雾、声音等传感器采集数据，用户通过手机APP获取到实时信息以监控室内情况，来保障家用电器的正常使用。<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 5. 基于Android平台的城市地标移动搜索开发 旅游业是陕西省的支柱产业，开发和旅游产业相关的移动视觉搜索应用具有重要的学术和应用价值。本项目研究基于移动视觉搜索的城市地标图像识别技术。通过城市地标的识别可以提供给游客更好的旅游体验，进一步挖掘城市的旅游资源，提升城市的形象。";
+    var as = document.getElementsByTagName("a");
+    var ps = document.getElementsByTagName("p");
+    ai_pre = "sakjdhak";
+    var change = new Array(true, true, true, true, true, true);
+    for (var i = 0; i < as.length; i++) {
+        (function(i) {
+
+            as[i].onclick = function() {
+                i = i - 7;
+                if (i == 0) {
+                    if (change[i]) {
+                        ps[i].innerHTML = ai;
+                        change[i] = false;
+                    } else {
+                        ps[i].innerHTML = ai_pre;
+                        change[i] = true;
+                    }
+                } else if (i == 1) {
+                    ps[i].innerHTML = share;
+                } else if (i == 3) {
+                    ps[i].innerHTML = fuhua;
+                } else if (i == 4) {
+                    ps[i].innerHTML = girl;
+                } else if (i == 5) {
+                    ps[i].innerHTML = creat;
+                }
+            }
+        })(i);
+    }
+}
+addLoadEvent(active_content);
+*/
