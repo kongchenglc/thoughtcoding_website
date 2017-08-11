386c45f046e14a0ccb3d1e80e7538996e1e81a90
鱼宁 <1578517699@qq.com>
2017-08-12 01:36:57 +0800

    写好了动态页面

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function() {
+            oldonload();
+            func();
+        }
+    }
+}
+
 function slide_top() {
     if (!document.getElementById)
         return false;
